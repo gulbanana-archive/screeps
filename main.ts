4ba9a20f2312768f8f54bf5ecf4eaa0a4db3095c
Thomas Castiglione <castiglione@mac.com>
2016-01-24 01:34:54 +0800

    limit spawns

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ import _ = require('lodash');
 function executeSpawns(specs: strategy.Spec[])
 {
     let spawners = [Game.spawns['Spawn1']];
+    let usedSpawners: Spawn[] = [];
     
     _.forEach(specs, spec =>
     {
@@ -15,12 +16,13 @@ function executeSpawns(specs: strategy.Spec[])
             console.log('spawn goal cost too high');
         }
         
-        let spawner = _.head(_.filter(eligibleSpawners, s => s.spawning == null && s.energy >= spec.cost));
+        let spawner = _.head(_.filter(eligibleSpawners, s => _.indexOf(usedSpawners, s)!=-1 && s.spawning == null && s.energy >= spec.cost));
         if (spawner)
         {
             let result = spawner.createCreep(spec.body, undefined, spec.memory);
             if (typeof(result) == 'string')
             {
+                usedSpawners.push(spawner);
                 console.log('spawning ' + result);
             }
             else
